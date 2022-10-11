7a69c3063a43b4eaa2c35d6f22c1f55963cd05c6
Asad <asadali047@gmail.com>
2022-10-12 00:52:27 +0400

    second release

--- runner.sh
+++ runner.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 
-streamlit run src/main.py --server.port 8080 --server.enableCORS false
+bash ~/miniconda.sh -b -p ~/miniconda
+
+export PATH="$HOME/miniconda/bin":$PATH
+pip install Streamlit
+conda install git
+
+git clone https://https://github.com/Asad1287/Reliability_Analyzer.git
+cd reliability_analyzer
+
+pip install -r src/requirements.txt
+sudo yum install tmux
+tmux
+streamlit run src/main.py 
